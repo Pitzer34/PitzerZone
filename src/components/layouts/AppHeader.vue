e31b63a2e559aeb8194a247762871816bc60cf7d
鍾弼合 <bh.zhong@yfy.com>
2024-08-30 17:46:09 +0800

    feat: 首頁 撰寫中

--- src/components/layouts/AppHeader.vue
+++ src/components/layouts/AppHeader.vue
@@ -4,12 +4,9 @@
   <header class="sticky top-0 left-0 z-20">
     <div class="shadow-sm shadow-gray-200 backdrop-blur-md dark:shadow-gray-600">
       <div class="flex items-center justify-between h-16 px-4 py-2 md:px-8">
-        <div class="w-auto">
-          <div>
-            <a href="">
-              <span>PitzerZone</span>
-            </a>
-          </div>
+        <div class="flex items-center h-12">
+          <div>mark</div>
+          <h1 class="text-xl font-bold">PitzerZone</h1>
         </div>
         <div class="flex items-center gap-2">
           <ul class="flex">
@@ -17,7 +14,7 @@
             <li>2</li>
             <li>3</li>
           </ul>
-          <div>5555</div>
+          <div>55</div>
           <ul class="flex">
             <li>1</li>
             <li>2</li>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,24 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
-  <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl py-6">
+  <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-5xl py-6">
     <div class="flex flex-col gap-8 m-6">
-      <div class="border-4 h-60">
-        content
+      <div class="">
+        <div class="flex gap-20">
+          <div class="bg-gray-400 w-60 h-60 text-center">
+            <img src="../assets/HomeMark.jpg" />
+          </div>
+          <div class="flex-1">
+            <div class="text-2xl font-bold mb-4">
+              PitzerZone
+            </div>
+            <div class="text-lg">
+              撰寫一些自我學習的小專案與技術筆記, 希望讓自己永遠保持動力, 歡迎參觀~~
+            </div>
+          </div>
+        </div>
       </div>
       <div class="border-2">
         toolbar1
